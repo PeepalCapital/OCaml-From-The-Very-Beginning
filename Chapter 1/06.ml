d71291f19aa478e5f7206fa0fdd4cd8a7c77a1bb
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-17 12:42:01 -0800

    Create 06.ml

---
+++ Chapter 1/06.ml
@@ -0,0 +1,9 @@
+utop # -3 mod -2;;
+- : int = -1
+
+It does division and finding remainders on negative numbers perhaps.
+
+utop # 0 mod 2;;
+- : int = 0
+
+This is simpler since any number times 0 is 0 and then the remainder is 0 again.
