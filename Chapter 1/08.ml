08d7d7e47b0a67b4e02f76d1862280833b558df9
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-17 12:47:03 -0800

    Create 08.ml

---
+++ Chapter 1/08.ml
@@ -0,0 +1,4 @@
+For alphabets be it lower or upper case probably the ASCII codes are used. Thus a comparison operator like > or < will yield appropriate results taking ASCII values.
+This is the same thing in C programming language too.
+
+In OCaml true is treated greater than false or in other words false is considered less than true. I didn't know that!
