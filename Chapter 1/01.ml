5ba43af9778b1d73d128bc720caf4742247a6d78
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-17 11:58:40 -0800

    Create 01.ml

---
+++ Chapter 1/01.ml
@@ -0,0 +1,36 @@
+1. What are the types of the following expressions and what do they evaluate to, and why?
+
+17
+1 + 2 * 3 + 4
+800 / 80 / 8
+400 > 200
+1 <> 1
+true || false
+true && false
+if true then false else true
+'%'
+'a' + 'b'
+
+----------
+
+
+- : int = 17
+
+- : int = 11
+
+- : int = 1
+
+- : bool = true
+
+- : bool = false
+
+- : bool = true
+
+- : bool = false
+
+- : bool = false
+
+- : char = '%'
+
+Error: The constant 'a' has type char but an expression was expected of type
+         int
