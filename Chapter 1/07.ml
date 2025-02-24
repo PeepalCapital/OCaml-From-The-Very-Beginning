b31528626bae9db60bc559b913dd9fa5fc684fe4
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-23 20:13:59 -0800

    Update 07.ml

--- Chapter 1/07.ml
+++ Chapter 1/07.ml
@@ -1,1 +1,6 @@
+Why not just use, for example, the integer 0 to represent false and the integer 1 for true? Why have
+a separate bool type at all?
+----
+
+
 This is probably because of making sure that if the number 1 or 0 if used as boolean don't change values during program execution.
