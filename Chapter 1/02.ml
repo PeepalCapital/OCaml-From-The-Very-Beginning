9947be34f14b3fa49d15abf5343d78cee1975bbb
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-23 20:12:35 -0800

    Update 02.ml

--- Chapter 1/02.ml
+++ Chapter 1/02.ml
@@ -1,3 +1,8 @@
+Consider the evaluations of the expressions 1 + 2 mod 3, (1 + 2) mod 3, and 1 + (2 mod 3). What
+can you conclude about the + and mod operators?
+----
+
+
 The mod operator takes precedence over +
 The () takes precendence over mod
 
