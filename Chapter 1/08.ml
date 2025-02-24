1f5a05dc2104df7cd06c07b3081616de39bc8c25
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-02-23 20:14:18 -0800

    Update 08.ml

--- Chapter 1/08.ml
+++ Chapter 1/08.ml
@@ -1,3 +1,9 @@
+What is the effect of the comparison operators like < and > on alphabetic values of type char? For
+example, what does 'p' < 'q' evaluate to? What is the effect of the comparison operators on the
+booleans, true and false?
+----
+
+
 For alphabets be it lower or upper case probably the ASCII codes are used. Thus a comparison operator like > or < will yield appropriate results taking ASCII values.
 This is the same thing in C programming language too.
 
